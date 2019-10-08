d79191fd9016e1449362ff1b3ba14c4d44e13353
ehornby <eric.hornby@gmail.com>
2019-10-08 16:52:45 -0500

    deployed backend

--- app-client/src/config.js
+++ app-client/src/config.js
@@ -3,6 +3,6 @@ export default {
         REGION: "ca-central-1",
         USER_POOL_ID: "ca-central-1_PywlFvZe9",
         APP_CLIENT_ID: "4lt6k8sejshhva5vnj8c4nr71f",
-        IDENTITY_POOL_ID: "ca-central-1:e3c4e3fb-56b5-4886-a489-1c49dd4d47cd"
+        IDENTITY_POOL_ID: "ca-central-1:cf00a9aa-6878-46e0-a2c2-9304e0f5a786"
     }
 }
