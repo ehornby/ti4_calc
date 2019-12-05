0573d7254906bce7469c0438b5a81e9120be9b7a
Eric Hornby <erich@frost-fighter.local>
2019-12-05 12:35:58 -0600

    version

--- func_app/src/components/Splash.js
+++ func_app/src/components/Splash.js
@@ -3,6 +3,6 @@ import React from 'react';
 export const Splash = () => (
     <div className='splash'>
         <h1>Please login or register to continue!</h1>
-        <h2>v2.1</h2>
+        <h2>v2.2</h2>
     </div>
 )
